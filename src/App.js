22b59c093150572247073543edf08b6e7f7bbb03
Mohamed <zer_moh@yahoo.com>
2024-12-10 09:10:30 -0500

    4th commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./pages/Menu";
+
+import AddTeacher from "./pages/AddTeacher";
+import UpdateTeacher from "./pages/UpdateTeacher";
+import DeleteTeacher from "./pages/DeleteTeacher";
+import FindTeacher from "./pages/FindTeacher";
+import ListOfTeachers from "./pages/ListOfTeachers";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
+
   return (
-    <>
-    <h1>List of employees</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Menu />}>
+          <Route path="AddTeacher" element={<AddTeacher />}></Route>
+          <Route path="UpdateTeacher" element={<UpdateTeacher />}></Route> 
+          <Route path="DeleteTeacher" element={<DeleteTeacher />}></Route>    
+          <Route path="FindTeacher" element={<FindTeacher />}></Route>   
+          <Route path="ListOfTeachers" element={<ListOfTeachers />}></Route> 
+          <Route path="*" element={<ErrorPage />}></Route> 
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
+
