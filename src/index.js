4e1266f76b015614f0e5554b807bdd4544c5cc2a
Mohamed <zer_moh@yahoo.com>
2024-12-12 08:55:35 -0500

    6th commit

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 //const express = require("express");
 const cors = require("cors");
-/*const app = express();
-app.use(cors({
+//const app = express();
+App.use(cors({
   origin:"https://widenskills.wuaze.com",
 }));
-*/
+
 
 root.render(
   <React.StrictMode>
