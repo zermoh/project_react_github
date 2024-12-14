837537f8db347cfcafc8954b2814ec0342f64c0e
Mohamed <zer_moh@yahoo.com>
2024-12-13 20:14:42 -0500

    6th commit

--- src/pages/ListOfTeachers.js
+++ src/pages/ListOfTeachers.js
@@ -10,7 +10,7 @@ const ListOfTeachers = () => {
     const handleSubmit = async() =>{
         console.log("==================2");
             const response = await fetch (
-                 "widenskills.mywebcommunity.org/React/ManageTeacher.php",
+                 "//widenskills.mywebcommunity.org/React/ManageTeacher.php",
                 {
                     method:"POST",
                     headers : {
