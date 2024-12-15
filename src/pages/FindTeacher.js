8b8f2cdf0797c4fd81b534480e946fda689a2ee2
Mohamed <zer_moh@yahoo.com>
2024-12-14 23:10:31 -0500

    6th commit

--- src/pages/FindTeacher.js
+++ src/pages/FindTeacher.js
@@ -10,13 +10,13 @@ const FindTeacher = () => {
     const handleSubmit = async() =>{
         console.log("==================2");
         const response = await fetch ("http://widenskills.mywebcommunity.org/React/TestList.php");
-        console.log(response);
+       /*  console.log(response);
                 
             const data = await response.json();
             for (let i=0;i<data.length;i++){
                 console.log(data[i].name);
             }
-            setData(data);
+            setData(data); */
         }   
         return (
 
