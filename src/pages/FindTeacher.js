7f7a83389418e0d4deb4ad23dced850a81bebd89
Mohamed <zer_moh@yahoo.com>
2024-12-14 23:02:35 -0500

    6th commit

--- src/pages/FindTeacher.js
+++ src/pages/FindTeacher.js
@@ -1,4 +1,31 @@
+import {useState,useEffect} from 'react';
 const FindTeacher = () => {
-    return (<h2>Find Teacher</h2>)
-}
-export default FindTeacher;
+
+   useEffect(() => {
+          handleSubmit();
+      },[]);
+    const[data,setData]=useState([]);
+    let[list,setList]=useState("");
+
+    const handleSubmit = async() =>{
+        console.log("==================2");
+        const response = await fetch ("http://widenskills.mywebcommunity.org/React/TestList.php");
+        console.log(response);
+                
+            const data = await response.json();
+            for (let i=0;i<data.length;i++){
+                console.log(data[i].name);
+            }
+            setData(data);
+        }   
+        return (
+
+       
+                <>
+                <p>Hello</p>
+               </> 
+           );
+         };
+       
+         export default FindTeacher;
+       
